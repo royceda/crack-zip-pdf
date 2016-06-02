9190d2cf9bd0d688ae8f4c1a11f835a17f151ecd
royceda <caucricity@gmail.com>
2016-06-02 20:28:37 +0200

    Update crunky.py

--- crunky.py
+++ crunky.py
@@ -1,50 +1,95 @@
 import string
-import threading
+from threading import Thread, RLock
 import sys
 import zipfile
-import pyPdf  
+#import pyPdf
+
 
 
 #pdf = pyPdf.PdfFileReader(open(toto.pdf))
-zfile       = zipfile.ZipFile('PDF.zip')
+
+
+lock        = RLock()
+zfile       = zipfile.ZipFile('Pi.zip')
 name        = zfile.namelist()
 thread      = []
 tab         = []
 charset_tab = [string.printable, string.ascii_lowercase, string.ascii_uppercase, string.ascii_letters,string.digits, string.hexdigits, string.punctuation]
-length_max  = 10
 
 
-def brute(string, length, charset, tab):
+
+length_max = 4
+password   = ''
+finished   = False
+
+print str(name)
+
+def brute(string, length, charset,tid):
+    global finished
+    global password
+
+    if finished == True:
+        with lock:
+            try:
+                zfile.read(name[0], pwd=password)
+                exit()
+            except:
+                finished == False
+
     if len(string) == length:
         return string
     for char in charset:
         temp = string + char
         try:
-            zfile.extractall(pwd=temp)
-            #pdf.decrypt(temp) 
-            for t in thread:
-                t.join()
-                print "password found !! "+string
-                return string
+            with lock:
+                zfile.read(name[0], pwd=password)
+                finished = True
+                #pdf.decrypt(temp)
+                print "Thread "+str(tid)+" found: "+temp
+                password = temp
+            exit()
         except:
-            print temp + "checked!"
-            tab.append(temp)
-            brute(temp, length, charset, tab)
+            qry = temp +" nThread "+str(tid)+" "+str(finished)+"\n"
+            #print qry
+            with lock:
+                tab.append(temp)
+            brute(temp, length, charset, tid)
 
 
 
-print charset_tab[0]
+class Brute(Thread):
+    def __init__(self, id, charset, length):
+        Thread.__init__(self)
+        self.id = id
+        self.charset = charset
+        self.length = length
+        self.init = ''
+        self.process = None
+
+    def run(self):
+        brute(self.init, self.length, self.charset, self.id)
+
+
+
 #for length in range(1, 8):
-for charset in charset_tab:
-    c = threading.Thread(None, brute, None,('', length_max, charset, tab))
+for i in range(0, len(charset_tab)):
+    c = Brute(i, charset_tab[i], length_max)
     thread.append(c)
-        
-for t in thread:
-    print "tread "+str(t)
-    t.start()
 
 for t in thread:
-    print "tread "+str(t)
+    print "thread "+str(t)
+    t.start()
+    print "thread "+str(t)
+
+
+while(finished == False):
+    a = 1;
+
+
+for t in thread:
+    print "thread "+str(t)
     t.join()
 
-print tab
+
+zfile.extractall(pwd=password)
+print "password: "+password
